1f1875c018ed6e97dc62bc4e8d7e7083223f8d17
Tsuyoshi Inoue <inoue0406@gmail.com>
2020-11-21 18:28:32 +0900

    added DWD preprocessing

---
+++ src/DWD_RYDL/tst_read_DWD.py
@@ -0,0 +1,30 @@
+import glob
+import pandas as pd
+import numpy as np
+import os
+import sys
+
+import matplotlib.pyplot as plt
+
+import h5py
+
+if __name__ == '__main__':
+    input_file = "../../data/DWD_RYDL/RYDL.hdf5"
+    print(input_file)
+    h5file = h5py.File(input_file,'r')
+    date_list =list(h5file.keys())
+    
+    for date in date_list:
+        R0 = h5file[date][()]
+        print("Processing Date:",date,np.mean(R0),np.max(R0))
+        plt.imshow(R0[:,:].astype(np.float32),vmin=0,vmax=5,origin='lower')
+        plt.savefig('../../result/DWD_RYDL/tstout/rain_figure_'+date+'.png')
+
+        
+
+        
+
+        
+
+
+
